7998ff002f11023f3b3e401a9ef5e03b4820a57a
Aayush Golecha <62604015+AayushGolecha@users.noreply.github.com>
2024-04-26 12:59:26 +0530

    updated file format

---
+++ chapter 5/array_methods.js
@@ -0,0 +1,16 @@
+let arr=[1,3,5,7,9]
+console.log(arr)
+
+// Array methods
+let a=arr.toString()
+console.log(a, typeof a)
+let b=arr.join(" and ")
+console.log(b, typeof b)
+let c=arr.pop()
+console.log(arr, c)
+let d=arr.push(11)
+console.log(arr, d)
+let e=arr.shift()
+console.log(arr, e)
+let f=arr.unshift(13)
+console.log(arr, f)
