90e48da02fed32aebaaea47f251dc4f2bf4f0977
Aayush Golecha <62604015+AayushGolecha@users.noreply.github.com>
2024-04-28 23:10:11 +0530

    exercise 2 completed

---
+++ Exercise 2/script.js
@@ -0,0 +1,55 @@
+console.log("Game Will Start Now")
+let com
+let obj = {
+    0: "S",
+    1: "W",
+    2: "G"
+}
+let i = 0
+let player
+let cScore = 0
+let pScore = 0
+while (i < 5) {
+    com = Math.floor(Math.random() * 3)
+    console.log(obj[com])
+    player = prompt("Choose b/w Snake, Water & Gun")
+    console.log(player)
+    if (obj[com] == player) {
+        cScore = cScore + 1
+        pScore = pScore + 1
+    }
+    else if (obj[com] == "S" && player == "W") {
+        cScore = cScore + 1
+    }
+    else if (obj[com] == "W" && player == "G") {
+        cScore = cScore + 1
+    }
+    else if (obj[com] == "G" && player == "S") {
+        cScore = cScore + 1
+    }
+    else if (obj[com] == "W" && player == "S") {
+        pScore = pScore + 1
+    }
+    else if (obj[com] == "G" && player == "W") {
+        pScore = pScore + 1
+    }
+    else {
+        pScore = pScore + p
+    }
+    i++
+}
+if(cScore==pScore){
+    console.log(`Player Score is ${pScore}`)
+    console.log(`Computer Score is ${cScore}`)
+    console.log("Match Draw")
+}
+else if(cScore>pScore){
+    console.log(`Player Score is ${pScore}`)
+    console.log(`Computer Score is ${cScore}`)
+    console.log("Computer Won")
+}
+else{
+    console.log(`Player Score is ${pScore}`)
+    console.log(`Computer Score is ${cScore}`)
+    console.log("Player Won")
+}
